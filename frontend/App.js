efb49b4a83e4ef7e19b046ef79dc06aeaae86031
magicenvision <rjrose2003@gmail.com>
2025-09-08 20:28:09 -0400

    added basic tailwind to text

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import "./global.css"
 
 export default function App() {
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-blue-500 bg-white">Universal React with Expo</Text>
+    <View className="flex bg-black h-full pt-14 px-4 ">
+      <View className="h-full">
+        <Text className="text-4xl font-semibold text-white">Todays Weather is</Text>
+        <Text className="text-5xl pt-2 text-white">Rideable</Text>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "white"
+
+    }
+  })
